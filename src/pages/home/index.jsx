bb990f800a516562da23c6ba468f63679ae0b8c5
theorivero <thrivero1@gmail.com>
2022-02-14 23:29:42 -0300

    Uses card car component to create the car cards grid at home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {DegradeButton} from "../../components/degradeButton";
 
 import './style.css'
-
+import MareaPic from '../../assets/images/homol_cars/download.jpeg'
+import {CarCard} from "../../components/carCard";
 
 export const Home = () => {
+
+    const [cars, setCars] = useState([
+        {
+        model: 'Marea Turbo',
+        price: 200000,
+        photo: MareaPic,
+        description: "explosivo vai de 0 a 100 em segundos!!!"
+        },
+        {
+        model: 'Marea Turbo',
+        price: 200000,
+        photo: MareaPic,
+        description: "explosivo vai de 0 a 100 em segundos"
+        },
+        {
+        model: 'Marea Turbo',
+        price: 200000,
+        photo: MareaPic,
+        description: "explosivo vai de 0 a 100 em segundos"
+        },
+        {
+            model: 'Marea Turbo',
+            price: 20000,
+            photo: MareaPic,
+            description: "explosivo vai de 0 a 100 em segundos!!!"
+        },
+        {
+            model: 'Marea Turbo',
+            price: 20000,
+            photo: MareaPic,
+            description: "explosivo vai de 0 a 100 em segundos explosivo vai de 0 a 100 em segundos explosivo vai de 0 a 100 em segundos explosivo vai de 0 a 100 em segundos"
+        },
+        {
+            model: 'Marea Turbo',
+            price: 20000,
+            photo: MareaPic,
+            description: "explosivo vai de 0 a 100 em segundos"
+        },
+
+    ])
+
     return (
         <div className="container">
             <div className="container-home">
@@ -19,7 +61,9 @@ export const Home = () => {
                 <div className="show-cars">
                     <h2>Nossos carros mais populares!</h2>
                     <div className="cars-grid">
-
+                        {cars.map(car =>
+                            <CarCard car={car}/>
+                        )}
                     </div>
                 </div>
             </div>
