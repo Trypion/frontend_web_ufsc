784678cb2467f1d2638694ce66d9c0a317ff153f
Israel <israel.schmitt.j@gmail.com>
2022-03-17 00:01:50 -0300

    car

--- src/components/carCard/index.jsx
+++ src/components/carCard/index.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import Popup from "../popup/Popup";
 import { useState } from "react";
 
-import {DegradeButton} from "../degradeButton";
+import { DegradeButton } from "../degradeButton";
 
-import "./style.css"
+import "./style.css";
 
-export const CarCard = ({car}) => {
-    const car_price_formated = car.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-    const [buttonPopup, setButtonPopup] = useState(false)
+export const CarCard = ({ car }) => {
+  const car_price_formated = car.price.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  const [buttonPopup, setButtonPopup] = useState(false);
 
-    return (
-        <div className="car-card">
-            <img src={car.photo} className="image-card-home" />
-            <h3>{car.modelo}</h3>
-            <div className="car-card-button-wrapper">
-                <DegradeButton className="btn-primary" onClick={() => setButtonPopup(true)}>Mais Informações</DegradeButton>
-            </div>
-            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                <img src={car.photo} alt="" />
-                <h3>{car.model}</h3>
-                <h4>{car_price_formated}</h4>
-                <p>{car.description}</p>
-            </Popup>
-        </div>
-    )
-}
+  return (
+    <div className="car-card">
+      <img src={car.photo} className="image-card-home" />
+      <h3>{car.modelo}</h3>
+      <div className="car-card-button-wrapper">
+        <DegradeButton
+          className="btn-primary"
+          onClick={() => setButtonPopup(true)}
+        >
+          Mais Informações
+        </DegradeButton>
+      </div>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <img src={car.photo} alt="" />
+        <h3>{car.model}</h3>
+        <h4>{car_price_formated}</h4>
+        <p>{car.description}</p>
+      </Popup>
+    </div>
+  );
+};
--- src/services/cars.js
+++ src/services/cars.js
@@ -15,3 +15,7 @@ export const findCar = async ({ id }) => {
 export const getAll = async () => {
   return api.get("/car");
 };
+
+export const deleteCar = async ({ id }) => {
+  return api.delete(`/car/${id}`);
+};
